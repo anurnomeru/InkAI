a0e3936fb9b8602330deeed6c369f5eeb0abda56
Anur <anurnomeru@gmail.com>
2026-03-28 11:44:19 +0800

    i18n: restore Chinese texts for clear/rebuild dialogs and logs; main: set logging encoding=utf-8 before imports

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # main.py
 # -*- coding: utf-8 -*-
+import logging
+logging.basicConfig(filename='app.log', filemode='a', level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S', encoding='utf-8')
 import customtkinter as ctk
 from ui import NovelGeneratorGUI
 
